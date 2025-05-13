0463191e433353f24565df89a228c28d393babb9
LEO TRIPP <leonardomandujano@gmail.com>
2025-05-13 16:20:56 -0700

    Sistema listo: trailas, work orders, auditoría y control de estatus

--- backend/routes/trailers.js
+++ backend/routes/trailers.js
@@ -6,7 +6,7 @@ router.use(express.json());
 
 // Obtener todas las trailas
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM trailas', (err, results) => {
+  db.query('SELECT * FROM trailers', (err, results) => {
     if (err) return res.status(500).send('Error al obtener trailas');
     res.json(results);
   });
@@ -24,7 +24,7 @@ router.get('/:nombre/work-orders', (req, res) => {
 router.put('/:nombre/estatus', (req, res) => {
   const { nombre } = req.params;
   const { estatus } = req.body;
-  db.query('UPDATE trailas SET estatus = ? WHERE nombre = ?', [estatus, nombre], err => {
+  db.query('UPDATE trailers SET estatus = ? WHERE nombre = ?', [estatus, nombre], err => {
     if (err) return res.status(500).send('Error al actualizar estatus');
     res.sendStatus(200);
   });
